0b33045e87287c931a9a9626f75eadaffd35333d
Hugo Uraga <hugouraga61@gmail.com>
2023-12-13 07:42:30 -0300

    fix: in memory database

--- src/application/use-cases/register-user.spec.ts
+++ src/application/use-cases/register-user.spec.ts
@@ -1,21 +1,18 @@
 import { InMemoryUserRepository } from '@/tests/repositories/in-memory-user.repository';
 import { it, describe, expect } from 'vitest';
 import { RegisterUserUseCase } from './register-user';
-import { PrismaRegisterUserRepository } from '@/infra/database/prisma/prisma-register-user.repository';
 
 describe('user creation', () => {
   it('should be possible for the user to register', async () => {
-    const userRepository = new PrismaRegisterUserRepository();
+    const userRepository = new InMemoryUserRepository();
     const registerUseCase = new RegisterUserUseCase(userRepository);
 
     const { user } = await registerUseCase.execute({
       name: 'Hugo Uraga',
-      email: 'hugouraga6111@gmail.com',
-      cpf: '118227604011',
+      email: 'hugouraga61@gmail.com',
+      cpf: '11822760402',
       password: '123456',
     });
-
-    console.log(user);
 
     expect(user.name).toBe('Hugo Uraga');
   });
