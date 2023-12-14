964259f8c7882a737adc597f090fae00384f70ce
Hugo Uraga <hugouraga61@gmail.com>
2023-12-14 15:46:09 -0300

    feat: add test in authenticate use case

--- src/application/use-cases/authenticate-user.spec.ts
+++ src/application/use-cases/authenticate-user.spec.ts
@@ -21,5 +21,36 @@ describe('user authentication', async () => {
     });
 
     expect(user.name).toBe('Hugo Uraga');
+    expect(user.email).toBe('hugouraga@gmail.com');
+  });
+
+  it('should not allow authentication for invalid email', async () => {
+    const userRepository = new InMemoryUserRepository();
+    const authenticateUseCase = new AuthenticateUseCase(userRepository);
+
+    expect(async () => {
+      await authenticateUseCase.execute({
+        email: 'hugouraga@gmail.com',
+        password: '123456',
+      });
+    }).rejects.toThrowError('invalid user credentials');
+  });
+
+  it('should not allow authentication for invalid password', async () => {
+    const userRepository = new InMemoryUserRepository();
+    const authenticateUseCase = new AuthenticateUseCase(userRepository);
+    await userRepository.save({
+      name: 'Hugo Uraga',
+      email: 'hugouraga@gmail.com',
+      cpf: '604.558.810-06',
+      password: await hash('123456', 6),
+    });
+
+    expect(async () => {
+      await authenticateUseCase.execute({
+        email: 'hugouraga@gmail.com',
+        password: '222222',
+      });
+    }).rejects.toThrowError('invalid user credentials');
   });
 });
