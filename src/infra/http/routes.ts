bacea1452a5771c92197061ff450e42dbb214021
Hugo Uraga <hugouraga61@gmail.com>
2024-01-09 15:41:26 -0300

    feat: add middleware to check jwt

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -2,9 +2,10 @@ import { me } from './controllers/me';
 import { register } from './controllers/user';
 import { authenticate } from './controllers/authenticate';
 import { FastifyInstance } from 'fastify';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/session', authenticate);
   app.get('/me', me);
-  app.post('/register/user', register);
+  app.post('/register/user', { onRequest: [verifyJWT] }, register);
 }
