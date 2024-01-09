8ef85aa3ba8e52d3dcc16538b06c94f8e9448ae6
Hugo Uraga <hugouraga61@gmail.com>
2024-01-09 15:23:30 -0300

    feat: add route check jwt

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,8 +1,10 @@
-import { FastifyInstance } from 'fastify';
+import { me } from './controllers/me';
 import { register } from './controllers/user';
 import { authenticate } from './controllers/authenticate';
+import { FastifyInstance } from 'fastify';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/session', authenticate);
+  app.get('/me', me);
   app.post('/register/user', register);
 }
