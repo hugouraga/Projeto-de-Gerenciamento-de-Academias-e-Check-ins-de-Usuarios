0985a13d4a9a6171706f260f0d2274f9bbf12b0f
Hugo Uraga <hugouraga61@gmail.com>
2024-01-09 14:33:50 -0300

    feat: register fastify jwt in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import fastify from 'fastify';
 import { appRoutes } from './infra/http/routes';
+import fastifyJwt from '@fastify/jwt';
+import 'dotenv';
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: '',
+});
 app.register(appRoutes);
